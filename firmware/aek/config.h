39a9e71e5d7c3818abb78e099dcdb51399f1c03d
djvs <2954343+djvs@users.noreply.github.com>
2021-09-28 23:52:22 -0400

    matrix

--- firmware/aek/config.h
+++ firmware/aek/config.h
@@ -20,15 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x1209
+#define PRODUCT_ID   0x9209
 #define DEVICE_VER   0x0001
 #define MANUFACTURER djvs
 #define PRODUCT      aek
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 11
 
 /*
  * Keyboard Matrix Assignments
@@ -40,9 +40,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
-#define UNUSED_PINS
+#define MATRIX_ROW_PINS { D7, B3, B7, D1, D3, D4, D5, D2, D0, B2, B1 }
+#define MATRIX_COL_PINS { F0, F1, F5, F6, F7, B6, B5, B4, D6, B0 }
+#define UNUSED_PINS { E6, C7 }
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
--- firmware/aek/keymaps/default/keymap.c
+++ firmware/aek/keymaps/default/keymap.c
@@ -30,13 +30,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_A,    KC_1,    MO(_FN),
-            KC_TAB,   KC_SPC
-    ),
+      KC_ESC     ,KC_F1      ,KC_F3      ,KC_F5      ,KC_F7      ,KC_F9      ,KC_F11     ,KC_PSCREEN ,KC_PAUSE                              ,\
+      KC_GRAVE   ,KC_2       ,KC_4       ,KC_6       ,KC_8       ,KC_0       ,KC_EQUAL   ,KC_HOME    ,KC_NUMLOCK ,KC_KP_SLASH               ,
+      KC_TAB     ,KC_W       ,KC_R       ,KC_Y       ,KC_J       ,KC_P       ,KC_RBRACKET,KC_END     ,KC_KP_7    ,KC_KP_9                   ,\
+      KC_CAPS    ,KC_S       ,KC_F       ,KC_H       ,KC_K       ,KC_SCOLON  ,KC_BSLASH  ,KC_UP      ,KC_KP_4    ,KC_KP_PLUS                ,\ 
+      KC_LSHIFT  ,KC_X       ,KC_V       ,KC_N       ,KC_COMMA   ,KC_SLASH   ,KC_ENTER   ,KC_DOWN    ,KC_KP_2    ,KC_KP_ENTER               ,\
+      KC_LCTRL   ,KC_LALT    ,KC_SPACE   ,KC_M       ,KC_RALT    ,KC_RGUI    ,KC_RCTRL   ,KC_RIGHT   ,KC_KP_DOT  ,KC_AUDIO_VOL_UP           ,\
+      KC_LGUI    ,KC_C       ,KC_B       ,KC_J       ,KC_DOT     ,KC_RSHIFT  ,KC_LEFT    ,KC_KP_0    ,KC_KP_3                               ,\
+      KC_Z       ,KC_D       ,KC_G       ,KC_U       ,KC_L       ,KC_QUOTE   ,KC_DELETE  ,KC_KP_1    ,KC_KP_6    ,KC_AUDIO_VOL_DOWN         ,\
+      KC_A       ,KC_E       ,KC_T       ,KC_7       ,KC_O       ,KC_LBRACKET,KC_INSERT  ,KC_PGDOWN  ,KC_KP_5    ,KC_KP_MINUS               ,\
+      KC_Q       ,KC_3       ,KC_5       ,            KC_9       ,KC_MINUS   ,KC_BSPACE  ,KC_PGUP    ,KC_KP_8    ,KC_KP_ASTERISK            ,\  
+      KC_F1      ,KC_F2      ,KC_F4      ,KC_F6      ,KC_F8      ,KC_F10     ,KC_F12     ,KC_SLCK    ,KC_KP_EQUAL,KC_MY_COMPUTER \
+    ) /*,
     [_FN] = LAYOUT(
         QMKBEST, QMKURL,  _______,
             RESET,    XXXXXXX
-    )
+    ) */
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
